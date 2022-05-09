c21e4dfe2ad8a6b5fe50d7775658496efd204444
Vinícius Cordeiro Pereira <viniciuscordeirop@gmail.com>
2022-05-09 02:20:46 +0200

    Create project boilerplate code

---
+++ Makefile
@@ -0,0 +1,57 @@
+.PHONY:	clean fclean re all
+
+NAME		:=	cub3d
+
+LIBFT		:=	libft
+
+CFLAGS		:=	-g3 -Wall -Wextra -Werror
+
+LFLAGS		:=	-Llibft -lft
+
+MINILIBX	:= -lmlx -lXext -lX11
+
+CC			:=	gcc
+
+SRC			:=	main.c 
+
+SRC_FOLDER 	:= src
+
+OBJ_FOLDER	:= obj
+
+SRCS 		:= $(addprefix ${SRC_FOLDER}/, ${SRC})
+
+OBJS 		:= $(addprefix ${OBJ_FOLDER}/, ${SRCS:.c=.o})
+
+.PHONY: all clean fclean re
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@ 
+
+all: $(NAME)
+
+$(NAME)	: $(OBJS)
+	make  -C $(LIBFT)
+	$(CC) $(CFLAGS) $(OBJS) $(LFLAGS) $(MINILIBX) -o $(NAME)
+
+$(OBJ_FOLDER)/%.o: %.c
+	@mkdir -p $(@D)
+	$(CC) $(CFLAGS) -c -o $@ $<
+
+clean:
+	make -C ${LIBFT} clean
+	rm -rf $(OBJ_FOLDER)
+
+fclean: clean
+	make -C ${LIBFT} fclean
+	rm -rf $(NAME)
+
+re: fclean ${NAME}
+
+do	: $(OBJS)
+	$(CC) $(CFLAGS) $(OBJS) -o $(NAME) $(LFLAGS)
+
+valgrind:
+	valgrind --leak-check=full ./cub3d ./maps/
+
+fullvalgrind:
+	valgrind --leak-check=full --show-leak-kinds=all ./minishell
---
+++ include/cub3d.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vcordeir <vcordeir@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/08 02:25:19 by vcordeir          #+#    #+#             */
+/*   Updated: 2022/05/08 02:27:12 by vcordeir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include "../libft/libft.h"
+
+
+#endif
