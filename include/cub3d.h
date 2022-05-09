61d0756cfc623cee6d9cea86a315b77bf38d267c
Vinícius Cordeiro Pereira <viniciuscordeirop@gmail.com>
2022-05-09 03:54:12 +0200

    First idea of game structures

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: vcordeir <vcordeir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/08 02:25:19 by vcordeir          #+#    #+#             */
-/*   Updated: 2022/05/08 02:27:12 by vcordeir         ###   ########.fr       */
+/*   Updated: 2022/05/09 03:53:39 by vcordeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,58 @@
 # include <fcntl.h>
 # include "../libft/libft.h"
 
+typedef enum e_enum_error
+{
+	E_SUCCESS,
+	E_INVALID_INPUT,
+	E_INVALID_FILE_EXTENSION,
+}	t_enum_error;
+
+typedef struct s_assets
+{
+	void	*player;
+	void	*wall;
+	void	*background;
+	void	*exit;
+	void	*collectible;
+}	t_assets;
+
+typedef struct s_player
+{
+	int	player_position_x;
+	int	player_position_y;
+	int	player_movements;
+}	t_player;
+
+typedef struct s_window
+{
+	int		img_width;
+	int		img_height;
+	void	*mlx;
+	void	*win;
+}	t_window;
+
+typedef struct s_scene
+{
+	char	**map;
+	char	*north_texture;
+	char	*south_texture;
+	char	*west_texture;
+	char	*east_texture;
+	char	*floor_color;
+	char	*ceilling_color;
+	int		map_height;
+	int		no_collectibles;
+	int		no_exits;
+}	t_scene;
+
+typedef struct s_game_set
+{
+	int			game_won;
+	t_scene		*scene;
+	t_assets	*assets;
+	t_player	*player;
+	t_window	*window;
+}	t_game_set;
 
 #endif
