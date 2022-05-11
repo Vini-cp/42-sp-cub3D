9cee373d375e570bf4a4233b7c9bf94afb833725
Vinícius Cordeiro Pereira <viniciuscordeirop@gmail.com>
2022-05-12 01:56:47 +0200

    Add new errors to print_error

--- src/utils/ft_print_error.c
+++ src/utils/ft_print_error.c
@@ -6,7 +6,7 @@
 /*   By: vcordeir <vcordeir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 03:43:27 by vcordeir          #+#    #+#             */
-/*   Updated: 2022/05/09 03:58:03 by vcordeir         ###   ########.fr       */
+/*   Updated: 2022/05/11 23:30:12 by vcordeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,18 @@ static char	*ft_search_error(int error_code)
 		return ("Invalid input, it should have only the map path");
 	else if (error_code == E_INVALID_FILE_EXTENSION)
 		return ("File extension must be '.cub'");
+	else if (error_code == E_FILE_DOES_NOT_EXISTS)
+		return ("Try to open a file that does not exists");
+	else if (error_code == E_MALLOC)
+		return ("No more space on the heap");
+	else if (error_code == E_MAP_HAS_EMPTY_LINES)
+		return ("The map must not be separeted by empy lines");
+	else if (error_code == E_MAP_HAS_INVALID_CHARS)
+		return ("There are only 6 possible characters: 0, 1, N, S, E and W");
+	else if (error_code == E_MAP_IS_OPEN)
+		return ("The map must be closed/surrounded by walls");
+	else if (error_code == E_MAP_HAS_NO_SPAWNING_POSITION)
+		return ("The map has no player position");
 	return ("No message error with this code.");
 }
 
